b1bd7b60414e92cd4a0c3193d2fa1e108945c11f
Dan Schwartz <daniel.robert.schwartz@gmail.com>
2019-03-16 14:55:42 -0400

    Part 1 of file-renaming

--- bert_erp_datasets/__init__.py
+++ bert_erp_datasets/__init__.py
@@ -1,7 +1,7 @@
 from . import colorless_green
 from . import corpus_base
 from . import data_loader
-from . import data_preparer
+from . import preprocessors
 from . import dataset
 from . import harry_potter
 from . import input_features
@@ -11,7 +11,7 @@ from . import university_college_london_corpus
 from .colorless_green import *
 from .corpus_base import *
 from .data_loader import *
-from .data_preparer import *
+from .preprocessors import *
 from .dataset import *
 from .harry_potter import *
 from .input_features import *
@@ -20,12 +20,12 @@ from .university_college_london_corpus import *
 
 
 __all__ = [
-    'colorless_green', 'corpus_base', 'data_loader', 'data_preparer', 'dataset', 'harry_potter', 'input_features',
+    'colorless_green', 'corpus_base', 'data_loader', 'preprocessors', 'dataset', 'harry_potter', 'input_features',
     'natural_stories', 'university_college_london_corpus']
 __all__.extend(colorless_green.__all__)
 __all__.extend(corpus_base.__all__)
 __all__.extend(data_loader.__all__)
-__all__.extend(data_preparer.__all__)
+__all__.extend(preprocessors.__all__)
 __all__.extend(dataset.__all__)
 __all__.extend(harry_potter.__all__)
 __all__.extend(input_features.__all__)
